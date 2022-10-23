228cc3e5acabaa97f401618fc8e15d20524f0989
cauachagas <caua.geof@gmail.com>
2022-10-23 06:15:07 -0400

    Adicionando droplet jenkins na região nyc3

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ terraform {
 provider "digitalocean" {
   token = "dop_v1_6789e84fda9c0a3558ece26be329877453acb60959a8c6496b43932987281520"
 }
+
+# Create a new Web Droplet in the nyc3 region
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins"
+  region   = "nyc3"
+  size     = "s-2vcpu-2gb"
+}
