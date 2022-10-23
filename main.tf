eaa09aaf7492e2a32ca16b36cd89039d7d0ddacf
cauachagas <caua.geof@gmail.com>
2022-10-23 07:09:17 -0400

    terraform apply após ssh_key e resource kubernetes

--- main.tf
+++ main.tf
@@ -24,3 +24,16 @@ resource "digitalocean_droplet" "jenkins" {
 data "digitalocean_ssh_key" "jornada" {
   name = "JornadaDevOpsAula3"
 }
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
+  region = "nyc3"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.4-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
