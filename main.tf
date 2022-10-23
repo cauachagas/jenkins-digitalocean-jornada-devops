6d85ccd7746b6197e562b978ebbe3ea09bff2c6e
cauachagas <caua.geof@gmail.com>
2022-10-23 07:01:06 -0400

    Change the token and ssh_key

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "dop_v1_6789e84fda9c0a3558ece26be329877453acb60959a8c6496b43932987281520"
+  token = "dop_v1_05de545e96ceeebdef7e017d3d57a1dec41b5973fefd8c5227622dba4cb2f3d9"
 }
 
 # Create a new Web Droplet in the nyc3 region
@@ -22,5 +22,5 @@ resource "digitalocean_droplet" "jenkins" {
 }
 
 data "digitalocean_ssh_key" "jornada" {
-  name = "JornadaDevOps"
+  name = "JornadaDevOpsAula3"
 }
